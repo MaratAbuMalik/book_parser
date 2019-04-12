871c8b82a2a27a84ab25462ea41d3ee588ff32e0
Marat Gadzhibutaev <rabbitjkee@gmail.com>
2019-04-12 20:29:33 +0300

    add bookmarks_header_lst and add all header structure to header_lst

--- pro_ramadan_handbook_parser.py
+++ pro_ramadan_handbook_parser.py
@@ -8,33 +8,40 @@ text_lst = ['']
 
 header_lst = ['']
 
+bookmarks_header_lst = ['']
+
 rb = xlrd.open_workbook('book.xlsx')
 sheet = rb.sheet_by_index(0)
 book_table = sheet.row_values
 
 
-def parse_section(row, col):
+def parse_section(row, col, header):
     section_dict = dict()
 
     section_dict['name'] = book_table(row)[col]
     if book_table(row)[col + 1] and not book_table(row)[col + 2]:
         section_dict['textIndex'] = row
         text_lst.append(book_table(row)[col + 1])
-        header_lst.append(book_table(row)[col])
+
+        bookmarks_header_lst.append(book_table(row)[col])
+        header_lst.append(header + '\n' + book_table(row)[col])
     else:
         section_dict['textIndex'] = 0
 
     section_dict['sections'] = []
 
     if book_table(row)[col + 2]:
-        section_dict['sections'].append(parse_section(row, col + 1))
+        header = header + '\n' + book_table(row)[col] if header != '' else book_table(row)[col]
+        section_dict['sections'].append(
+            parse_section(row, col + 1, header + '.' if 'ProRamadan' not in header else ''))
         while(True):
             row += 1
             if row == sheet.nrows or col >= get_level(row):
                 break
 
             if book_table(row)[col + 1]:
-                section_dict['sections'].append(parse_section(row, col + 1))
+                section_dict['sections'].append(
+                    parse_section(row, col + 1, header + '.' if 'ProRamadan' not in header else ''))
 
     return section_dict
 
@@ -84,27 +91,18 @@ def get_text_headers():
     return text
 
 
+def get_bookmarks_headers():
+    text = 'List<String> bookmarksTextHeaders = ['
 
-def generate_text(lst):
-    xml_begin = '<?xml version="1.0" encoding="utf-8"?>'
-    root = etree.Element('resources')
-    string_array = etree.Element('string-array', name='textArray', formatted='false')
-    root.append(string_array)
+    for i in bookmarks_header_lst:
+        text += f"'''{i}''',\n"
 
-    item = etree.Element('item')
-    item.text = "<![CDATA[" + "empty element" + "]]>"
-    string_array.append(item)
+    text += '];'
 
-    for text in lst:
-        item = etree.Element('item')
-        item.text = "<![CDATA[" + text + "]]>"
-        string_array.append(item)
-
-    s = str(etree.tostring(root, pretty_print=True, encoding=str))
-    open('text.xml', 'w', encoding='UTF-8').write(xml_begin + '\n' + unescape(s))
+    return text
 
 
-book_data = parse_section(1, 0)
+book_data = parse_section(1, 0, '')
 
 print(len(header_lst))
 print(len(text_lst))
@@ -112,4 +110,5 @@ with open('pro_ramadan_handbook_structure.dart', 'w', encoding='utf-8') as f:
     f.write(f"import '../util/section.dart'; Section handbookStructure = {get_structure(book_data)};")
 with open('pro_ramadan_handbook_text.dart', 'w', encoding='utf-8') as f:
     f.write(get_text_headers())
+    f.write(get_bookmarks_headers())
     f.write(get_text())
