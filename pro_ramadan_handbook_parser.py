01bd00baed36464a041d7284224d156b9f865ac8
Marat Gadzhibutaev <rabbitjkee@gmail.com>
2020-03-16 20:08:57 +0300

    add share_text parsing

--- pro_ramadan_handbook_parser.py
+++ pro_ramadan_handbook_parser.py
@@ -10,12 +10,30 @@ header_lst = ['']
 
 bookmarks_header_lst = ['']
 
-rb = xlrd.open_workbook('book.xlsx')
+rb = xlrd.open_workbook('text.xlsx')
 sheet = rb.sheet_by_index(0)
 book_table = sheet.row_values
 
 
-def parse_section(row, col, header, prefix = ''):
+def parce_text(file):
+    global rb
+    global sheet
+    global book_table
+    global text_lst
+    global header_lst
+    global bookmarks_header_lst
+
+    rb = xlrd.open_workbook(file)
+    sheet = rb.sheet_by_index(0)
+    book_table = sheet.row_values
+    text_lst = ['']
+    header_lst = ['']
+    bookmarks_header_lst = ['']
+
+    return parse_section(1, 0, '')
+
+
+def parse_section(row, col, header, prefix=''):
     section_dict = dict()
 
     section_dict['name'] = prefix + book_table(row)[col]
@@ -33,7 +51,7 @@ def parse_section(row, col, header, prefix = ''):
     if book_table(row)[col + 2]:
         header = header + '\n' + book_table(row)[col] if header != '' else book_table(row)[col]
         section_dict['sections'].append(
-            parse_section(row, col + 1, header + '.' if 'ProRamadan' not in header else '',
+            parse_section(row, col + 1, header + '\n' if 'ProRamadan' not in header else '',
                           prefix=(prefix.strip() + str(len(section_dict['sections']) + 1) + '. ')))
         while(True):
             row += 1
@@ -42,8 +60,9 @@ def parse_section(row, col, header, prefix = ''):
 
             if book_table(row)[col + 1]:
                 section_dict['sections'].append(
-                    parse_section(row, col + 1, header + '.' if 'ProRamadan' not in header else '',
-                                  prefix=(prefix.strip() + str(len(section_dict['sections']) + 1) + '. ')))
+                    parse_section(row, col + 1, header + '\n' if 'ProRamadan' not in header else '',
+                                  prefix=(prefix.strip() + str(len(section_dict['sections']) + 1)
+                                          + '. ')))
 
     return section_dict
 
@@ -104,13 +123,29 @@ def get_bookmarks_headers():
     return text
 
 
-book_data = parse_section(1, 0, '')
+def get_share_text():
+    text = 'List<String> handbookShareText = ['
+
+    for i in range(len(header_lst)):
+        text += f"'''{header_lst[i]}\n\n{text_lst[i]}''',\n"
+
+    text += '];'
+
+    return text
+
+
+book_data = parce_text(file='text.xlsx')
 
 print(len(header_lst))
 print(len(text_lst))
 with open('structure.dart', 'w', encoding='utf-8') as f:
-    f.write(f"import '../util/section.dart'; Section handbookStructure = {get_structure(book_data)};")
+    f.write(f"import '../util/util_classes.dart'; Section handbookStructure = {get_structure(book_data)};")
 with open('text.dart', 'w', encoding='utf-8') as f:
     f.write(get_text_headers())
     f.write(get_bookmarks_headers())
     f.write(get_text())
+
+
+share_book_data = parce_text(file='share_text.xlsx')
+with open('share_text.dart', 'w', encoding='utf-8') as f:
+    f.write(get_share_text())
